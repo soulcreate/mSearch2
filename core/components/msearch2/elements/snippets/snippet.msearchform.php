5e47fbbff26c3009ac5660650113435252e4301c
bezumkin <bezumkin@yandex.ru>
2015-03-05 22:38:33 +0700

    Fixed #34 - &autocomplete=`0` in mSearchForm Version 1.2.5-pl

--- core/components/msearch2/elements/snippets/snippet.msearchform.php
+++ core/components/msearch2/elements/snippets/snippet.msearchform.php
@@ -12,7 +12,7 @@ $mSearch2 = new mSearch2($modx, array(), $pdoFetch);
 $mSearch2->initialize($modx->context->key);
 
 $config = array(
-	'autocomplete' => !empty($autocomplete) ? $autocomplete : 'results',
+	'autocomplete' => !empty($autocomplete) ? $autocomplete : '',
 	'queryVar' => !empty($queryVar) ? $queryVar : 'query',
 	'minQuery' => !empty($minQuery) ? (integer) $minQuery : 3,
 	'pageId' => !empty($pageId) ? (integer) $pageId : $modx->resource->id,
@@ -22,28 +22,30 @@ $scriptProperties = array_merge($scriptProperties, $config);
 if (empty($tplForm)) {$tplForm = 'tpl.mSearch2.form';}
 $form = $pdoTools->getChunk($tplForm, $scriptProperties);
 
-$hash = sha1(serialize($scriptProperties));
-$_SESSION['mSearch2'][$hash] = $scriptProperties;
+if (!empty($config['autocomplete'])) {
+	$hash = sha1(serialize($scriptProperties));
+	$_SESSION['mSearch2'][$hash] = $scriptProperties;
 
-$form = str_ireplace('<form', '<form data-key="'.$hash.'"', $form);
-// Place for enabled log
-if ($modx->user->hasSessionContext('mgr') && !empty($showLog)) {
-	$form = str_ireplace('</form>', "</form>\n<pre class=\"mSearchFormLog\"></pre>", $form);
+	$form = str_ireplace('<form', '<form data-key="'.$hash.'"', $form);
+	// Place for enabled log
+	if ($modx->user->hasSessionContext('mgr') && !empty($showLog)) {
+		$form = str_ireplace('</form>', "</form>\n<pre class=\"mSearchFormLog\"></pre>", $form);
+	}
+
+	// Setting values for frontend javascript
+	$main_config = array(
+		'cssUrl' => $mSearch2->config['cssUrl'].'web/',
+		'jsUrl' => $mSearch2->config['jsUrl'].'web/',
+		'actionUrl' => $mSearch2->config['actionUrl'],
+	);
+
+	$modx->regClientStartupScript('
+	<script type="text/javascript">
+		if (typeof mse2Config == "undefined") {mse2Config = '.$modx->toJSON($main_config).';}
+		if (typeof mse2FormConfig == "undefined") {mse2FormConfig = {};}
+		mse2FormConfig["'.$hash.'"] = '.$modx->toJSON($config).';
+	</script>'
+	, true);
 }
-
-// Setting values for frontend javascript
-$main_config = array(
-	'cssUrl' => $mSearch2->config['cssUrl'].'web/',
-	'jsUrl' => $mSearch2->config['jsUrl'].'web/',
-	'actionUrl' => $mSearch2->config['actionUrl'],
-);
-
-$modx->regClientStartupScript('
-<script type="text/javascript">
-if (typeof mse2Config == "undefined") {mse2Config = '.$modx->toJSON($main_config).';}
-if (typeof mse2FormConfig == "undefined") {mse2FormConfig = {};}
-mse2FormConfig["'.$hash.'"] = '.$modx->toJSON($config).';
-</script>'
-, true);
 
 return $form;
